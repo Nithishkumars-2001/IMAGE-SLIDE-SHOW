2c99d8e9ede3d2d7ceb474f32556b25371ba4c91
Nithishkumars-2001 <nithishkumar70102@gmail.com>
2024-07-26 10:18:47 +0530

    Updated all files

---
+++ script.js
@@ -0,0 +1,29 @@
+
+// select container
+
+var container = document.querySelector(".container")
+
+function img2()
+{
+    container.style.backgroundImage = "url(2.webp)"
+}
+function img3()
+{
+    container.style.backgroundImage = "url(3.jpg)"
+}
+function img4()
+{
+    container.style.backgroundImage = "url(4.webp)"
+}
+function img5()
+{
+    container.style.backgroundImage = "url(5.jpg)"
+}
+function img6()
+{
+    container.style.backgroundImage = "url(6.jpg)"
+}
+function img7()
+{
+    container.style.backgroundImage = "url(7.jpg)"
+}
